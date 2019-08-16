0202c4de8d79af99eab944b60f91f86a5e9feac7
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2019-08-16 08:50:28 -0400

    create_loops.py added

--- src/shared_sql_code.py
+++ src/shared_sql_code.py
@@ -30,5 +30,22 @@ def output_data(db_name, tbl_name, csv_file):
         '--csv',
         db_name,
         f'.output {csv_file}',
-        f'SELECT * FROM {tbl_name};',
+        f'SELECT * FROM {tbl_name};'
+    ])
+
+
+def output_distinct_data(db_name, tbl_name, csv_file):
+    '''Helper function to outport data to a CSV file from a SQLite3 database.
+
+    db_name -- database name
+    tbl_name -- table in database to load data into
+    csv_file -- CSV file
+    '''
+    subprocess.run([
+        'sqlite3',
+        '--echo',
+        '--csv',
+        db_name,
+        f'.output {csv_file}',
+        f'SELECT DISTINCT * FROM {tbl_name};'
     ])
