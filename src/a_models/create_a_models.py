c85718433fc8afc8a181d37e9525e38aac598d47
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2019-09-03 14:25:23 -0400

    Break time - finished doing A data loading

--- src/a_models/create_a_models.py
+++ src/a_models/create_a_models.py
@@ -1,4 +1,5 @@
 import triangulation.src.shared_code.file_names as file_names
+import triangulation.src.shared_code.column_names as columns
 import triangulation.src.shared_code.shared_code as shared_code
 import triangulation.src.shared_code.table_names as table_names
 import sqlite3
@@ -8,45 +9,73 @@ def in_data_tables(cur, database_name):
     """
 
     """
+
+    # Create all of the tables
     cur.execute(f'''
         CREATE TABLE {table_names.pik_data} (
-            prdn TEXT NOT NULL,
-            grant_yr INTEGER NOT NULL,
-            app_yr INTEGER NOT NULL,
-            inv_seq INTEGER NOT NULL,
-            pik TEXT NOT NULL,
-            ein TEXT NOT NULL,
-            firmid TEXT NOT NULL,
-            emp_yr INTEGER NOT NULL
+            {columns.prdn.cmd},
+            {columns.grant_yr.cmd},
+            {columns.app_yr.cmd},
+            {columns.inv_seq.cmd},
+            {columns.pik.cmd},
+            {columns.ein.cmd},
+            {columns.firmid.cmd},
+            {columns.emp_yr.cmd}
         );
         CREATE TABLE {table_names.ein_data} (
-            prdn TEXT NOT NULL,
-            grant_yr INTEGER NOT NULL,
-            ass_seq INTEGER NOT NULL,
-            ein TEXT,
-            firmid TEXT NOT NULL,
-            crosswalk_yr INTEGER NOT NULL,
-            pass_no INTEGER NOT NULL
+            {columns.prdn.cmd},
+            {columns.grant_yr.cmd},
+            {columns.assg_seq.cmd},
+            {columns.ein.cmd},
+            {columns.firmid.cmd},
+            {columns.cw_yr.cmd},
+            {columns.pass_no.cmd}
         );
         CREATE TABLE {table_names.assignee_info} (
-            prdn TEXT NOT NULL,
-            ass_seq INTEGER NOT NULL,
-            ass_type TEXT,
-            ass_st TEXT,
-            ass_ctry TEXT
+            {columns.prdn.cmd},
+            {columns.assg_seq.cmd},
+            {columns.assg_type.cmd},
+            {columns.assg_st.cmd},
+            {columns.assg_ctry.cmd}
         );
         CREATE TABLE {table_names.prdn_metadata} (
-            prdn TEXT NOT NULL,
-            grant_yr INTEGER NOT NULL,
-            app_yr INTEGER,
-            num_assg INTEGER,
-            us_inventor_flag INTEGER
+            {columns.prdn.cmd},
+            {columns.grant_yr.cmd},
+            {columns.app_yr.cmd},
+            {columns.num_assg.cmd},
+            {columns.us_inv_flag.name.cmd}
         );
     ''')
+
+    # Load the data in
     shared_code.import_data(database_name, table_names.pik_data, file_names.pik_data_csvfile)
     shared_code.import_data(database_name, table_names.ein_data, file_names.ein_data_csvfile)
     shared_code.import_data(database_name, table_names.assignee_info, file_names.assignee_info_csvfile)
     shared_code.import_data(database_name, table_names.prdn_metadata, file_names.prdn_metadata_csvfile)
+
+    # Make the indexes
+    cur.execute(f'''
+        CREATE INDEX
+            ein_idx_prdn_ein_firmid_idx
+        ON
+            {table_names.ein_data}({columns.prdn.name}, {columns.ein.name}, {columns.firmid.name});
+        CREATE INDEX
+            pik_idx_prdn_ein_firmid_idx
+        ON
+            {table_names.pik_data}({columns.prdn.name}, {columns.ein.name}, {columns.firmid.name});
+        CREATE INDEX
+            ein_idx_prdn_as_idx
+        ON
+            {table_names.ein_data}({columns.prdn.name}, {columns.assg_seq.name});
+        CREATE INDEX
+            assg_info_prdn_as_idx
+        ON
+            {table_names.assignee_info}({columns.prdn.name}, {columns.assg_seq.name});
+        CREATE INDEX
+            prdn_metadata_main_idx
+        ON
+            {table_names.prdn_metadata}({columns.prdn.name});
+    ''')
 
 
 def make_a_models(database_name):
@@ -55,4 +84,5 @@ def make_a_models(database_name):
     """
     conn = sqlite3.connect(database_name)
     cur = conn.cursor()
+    cur.execute('pragma temp_store = MEMORY;')  # /tmp was filling up - the PRAGMA seems to take care of that
     in_data_tables(cur, database_name)
---
+++ src/shared_code/table_names.py
@@ -0,0 +1,5 @@
+# Table name
+assignee_info = 'assignee_info'
+ein_data = 'ein_data'
+pik_data = 'pik_data'
+prdn_metadata = 'prdn_metadata'
