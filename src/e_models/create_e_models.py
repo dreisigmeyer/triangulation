4bfe9ccf8545b7bd3a54b6ea8ee21ac71a85bbc0
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2020-01-07 09:54:30 -0500

    Starting e_models

---
+++ src/e_models/create_e_models.py
@@ -0,0 +1,5 @@
+import triangulation.src.shared_code.column_names as columns
+import triangulation.src.shared_code.file_names as file_names
+import triangulation.src.shared_code.shared_code as shared_code
+import triangulation.src.shared_code.table_names as table_names
+
