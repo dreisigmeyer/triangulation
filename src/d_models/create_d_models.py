f4880970977410e43bd94c1f76f169c3faab7be6
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2020-02-04 15:03:50 -0500

    Start D1 model

--- src/d_models/create_d_models.py
+++ src/d_models/create_d_models.py
@@ -10,6 +10,72 @@ def make_aux_tables(fh):
     """
     fh.write(
         f'''
+CREATE TABLE {table_names.name_match_prdn_assg_num} (
+    {columns.prdn.cmd},
+    {columns.assg_seq.cmd},
+    UNIQUE (
+        {columns.prdn.name},
+        {columns.assg_seq.name}
+    )
+);
+
+CREATE TABLE {table_names.firmid_seq_data} (
+    {columns.prdn.cmd},
+    {columns.assg_seq.cmd},
+    {columns.firmid.cmd},
+    UNIQUE (
+        {columns.prdn.name},
+        {columns.assg_seq.name},
+        {columns.firmid.name}
+    )
+);
+
+CREATE TABLE {table_names.firmid_name_data} (
+    {columns.firm_name.cmd},
+    {columns.firmid.cmd},
+    UNIQUE (
+        {columns.firm_name.name},
+        {columns.firmid.name}
+    )
+);
+
+INSERT INTO name_match_prdn_assg_num
+SELECT DISTINCT xml_pat_num, assg_num
+FROM name_match;
+
+INSERT INTO firmid_seq_data
+SELECT DISTINCT prdn, assg_seq, firmid
+FROM a1_models;
+
+INSERT INTO firmid_seq_data
+SELECT DISTINCT prdn, assg_seq, firmid
+FROM a2_models;
+
+INSERT INTO firmid_seq_data
+SELECT DISTINCT prdn, assg_seq, firmid
+FROM a3_models;
+
+INSERT INTO firmid_name_data
+SELECT DISTINCT assignee_name_data.name, firmid_seq_data.firmid
+FROM assignee_name_data, firmid_seq_data
+WHERE
+    assignee_name_data.xml_pat_num = firmid_seq_data.prdn AND
+    assignee_name_data.assg_num = firmid_seq_data.assg_seq;
+
+DELETE FROM firmid_name_data
+WHERE firm_name IN (
+    SELECT firm_name
+    FROM firmid_name_data
+    GROUP BY firm_name
+    HAVING COUNT(*) > 1
+);
+
+DELETE FROM firmid_name_data
+WHERE firm_name IN (
+    SELECT firm_name
+    FROM firmid_name_data
+    WHERE firmid = ''
+);
     ''')
 
 
