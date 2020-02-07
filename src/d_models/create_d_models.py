02ea561e1079584c19bebce56099f31245755d6a
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2020-02-07 13:45:34 -0500

    Finishing creating D model aux tables

--- src/d_models/create_d_models.py
+++ src/d_models/create_d_models.py
@@ -39,42 +39,58 @@ CREATE TABLE {table_names.firmid_name_data} (
     )
 );
 
-INSERT INTO name_match_prdn_assg_num
-SELECT DISTINCT xml_pat_num, assg_num
-FROM name_match;
+INSERT INTO {table_names.name_match_prdn_assg_num}
+SELECT DISTINCT
+    {columns.xml_pat_num.name},
+    {columns.assg_seq.name}
+FROM
+    {table_names.name_match};
 
-INSERT INTO firmid_seq_data
-SELECT DISTINCT prdn, assg_seq, firmid
-FROM a1_models;
+INSERT INTO {table_names.firmid_seq_data}
+SELECT DISTINCT
+    {columns.prdn.name},
+    {columns.assg_seq.name},
+    {columns.firmid.name}
+FROM {table_names.a1_models};
 
-INSERT INTO firmid_seq_data
-SELECT DISTINCT prdn, assg_seq, firmid
-FROM a2_models;
+INSERT INTO {table_names.firmid_seq_data}
+SELECT DISTINCT
+    {columns.prdn.name},
+    {columns.assg_seq.name},
+    {columns.firmid.name}
+FROM {table_names.a2_models};
 
-INSERT INTO firmid_seq_data
-SELECT DISTINCT prdn, assg_seq, firmid
-FROM a3_models;
+INSERT INTO {table_names.firmid_seq_data}
+SELECT DISTINCT
+    {columns.prdn.name},
+    {columns.assg_seq.name},
+    {columns.firmid.name}
+FROM {table_names.a3_models};
 
-INSERT INTO firmid_name_data
-SELECT DISTINCT assignee_name_data.name, firmid_seq_data.firmid
-FROM assignee_name_data, firmid_seq_data
+INSERT INTO {table_names.firmid_name_data}
+SELECT DISTINCT
+    {table_names.assignee_name_data}.{columns.assg_name.name},
+    {table_names.firmid_seq_data}.{columns.firmid.name}
+FROM
+    {table_names.assignee_name_data},
+    {table_names.firmid_seq_data}
 WHERE
-    assignee_name_data.xml_pat_num = firmid_seq_data.prdn AND
-    assignee_name_data.assg_num = firmid_seq_data.assg_seq;
+    {table_names.assignee_name_data}.{columns.xml_pat_num.name} = {table_names.firmid_seq_data}.{columns.prdn.name} AND
+    {table_names.assignee_name_data}.{columns.assg_seq.name} = {table_names.firmid_seq_data}.{columns.assg_seq.name};
 
-DELETE FROM firmid_name_data
-WHERE firm_name IN (
-    SELECT firm_name
-    FROM firmid_name_data
-    GROUP BY firm_name
+DELETE FROM {table_names.firmid_name_data}
+WHERE {columns.firm_name.name} IN (
+    SELECT {columns.firm_name.name}
+    FROM {table_names.firmid_name_data}
+    GROUP BY {columns.firm_name.name}
     HAVING COUNT(*) > 1
 );
 
-DELETE FROM firmid_name_data
-WHERE firm_name IN (
-    SELECT firm_name
-    FROM firmid_name_data
-    WHERE firmid = ''
+DELETE FROM {table_names.firmid_name_data}
+WHERE {columns.firm_name.name} IN (
+    SELECT {columns.firm_name.name}
+    FROM {table_names.firmid_name_data}
+    WHERE {columns.firmid.name} = ''
 );
     ''')
 
