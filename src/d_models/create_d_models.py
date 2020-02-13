22b95e0a3a8edb16a302841bf629b33a5b23c89a
David Dreisigmeyer <dwdreisigmeyer@icloud.com>
2020-02-13 09:48:39 -0500

    Starting possible D models

--- src/d_models/create_d_models.py
+++ src/d_models/create_d_models.py
@@ -95,6 +95,66 @@ WHERE {columns.firm_name.name} IN (
     ''')
 
 
+def make_possible_d_models(fh):
+    """
+
+    """
+    fh.write(
+        f'''
+CREATE TABLE {table_names.possible_d_models} (
+    {columns.prdn.cmd},
+    {columns.count.cmd},
+    {columns.assg_seq.cmd},
+    {columns.cw_yr.cmd},
+    {columns.emp_yr.cmd},
+    {columns.firmid.cmd},
+    {columns.grant_yr.cmd},
+    {columns.app_yr.cmd},
+    {columns.assg_ctry.cmd},
+    {columns.assg_st.cmd},
+    {columns.assg_type.cmd},
+    {columns.us_inv_flag.cmd},
+    {columns.mult_assg_flag.cmd},
+    {columns.model.cmd},
+    {columns.uniq_firmid.cmd},
+    {columns.assg_name.cmd}
+)
+
+INSERT INTO {table_names.possible_d_models} (
+    {columns.prdn.name},
+    {columns.assg_seq.name},
+    {columns.grant_yr.name},
+    {columns.app_yr.name},
+    {columns.assg_ctry.name},
+    {columns.assg_st.name},
+    {columns.assg_type.name},
+    {columns.us_inv_flag.name},
+    {columns.mult_assg_flag.name},
+    {columns.assg_name.name}
+)
+SELECT
+    {table_names.assignee_name_data}.{columns.xml_pat_num.name},
+    {table_names.assignee_name_data}.{columns.assg_seq.name},
+    {table_names.assignee_name_data}.{columns.grant_yr.name},
+    {table_names.prdn_metadata}.{columns.app_yr.name},
+    {table_names.assignee_info}.{columns.assg_ctry.name},
+    {table_names.assignee_info}.{columns.assg_st.name},
+    {table_names.assignee_info}.{columns.assg_type.name},
+    {table_names.prdn_metadata}.{columns.us_inv_flag.name},
+    {table_names.prdn_metadata}.{columns.mult_assg_flag.name},
+    {table_names.assignee_name_data}.{columns.assg_name.name}
+FROM
+    {table_names.assignee_name_data},
+    {table_names.assignee_info},
+    {table_names.prdn_metadata}
+WHERE {table_names.assignee_name_data}.{columns.assg_name.name} IN (
+    SELECT {columns.firm_name} FROM {table_names.firmid_name_data}) AND
+    {table_names.assignee_name_data}.{columns.xml_pat_num.name} = {table_names.assignee_info}.{columns.prdn.name} AND
+    {table_names.assignee_name_data}.{columns.assg_seq.name} = {table_names.assignee_info}.{columns.assg_seq.name} AND
+    {table_names.assignee_name_data}.{columns.xml_pat_num.name} = {table_names.prdn_metadata}.{columns.prdn.name};
+    ''')
+
+
 def generate_d_model_sql_script(sql_script_fn, assignee_years):
     """
 
@@ -103,3 +163,4 @@ def generate_d_model_sql_script(sql_script_fn, assignee_years):
         shared_code.model_header(f)
         shared_code.in_data_tables(f, 'D', assignee_years)
         make_aux_tables(f)
+        make_possible_d_models(f)
